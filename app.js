30d92072b43b39e8edea519b833adb5739da68ec
olivercastroteran <olivercastroteran@gmail.com>
2020-03-18 22:45:01 -0400

    Sending, rendering and deleting data from database functionality

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 // Create element on cafe UI rendering it
 function renderCafe(doc) {
   let li = document.createElement('li');
   let name = document.createElement('span');
   let city = document.createElement('span');
+  let cross = document.createElement('div');
 
   li.setAttribute('data-id', doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = 'x';
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
+
+  // Deleting data
+  cross.addEventListener('click', e => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id');
+    db.collection('cafes')
+      .doc(id)
+      .delete();
+  });
 }
 
+// Getting Data
 db.collection('cafes')
   .get()
   .then(snapshot => {
@@ -23,3 +37,14 @@ db.collection('cafes')
       renderCafe(doc);
     });
   });
+
+// Saving Data
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  db.collection('cafes').add({
+    name: form.name.value,
+    city: form.city.value
+  });
+  form.name.value = '';
+  form.city.value = '';
+});
